041c163e8de1c667c2549c1be3605101b8d6273b
Chen Lei <my@mysq.to>
2021-03-29 23:00:05 +0800

    Update grc.fish

--- conf.d/grc.fish
+++ conf.d/grc.fish
@@ -4,7 +4,7 @@ set -l execs cat cvs df diff dig gcc g++ ls ifconfig \
        wdiff blkid du dnf docker docker-compose docker-machine env id ip iostat journalctl kubectl \
        last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
        fdisk findmnt free semanage sar ss sysctl systemctl stat showmount \
-       tcpdump tune2fs vmstat w who sockstat
+       tcpdump tune2fs vmstat w who sockstat who
 
   if set -q grc_plugin_execs
     set execs $grc_plugin_execs
