5a71cca56c8631929306e17ae031554fd918ebcc
Chen Lei <my@mysq.to>
2021-03-29 22:58:06 +0800

    Update grc.fish

--- conf.d/grc.fish
+++ conf.d/grc.fish
@@ -1,7 +1,10 @@
 if type -q grc
-  set -l execs cat cvs df diff dig gcc g++ ls ifconfig \
-               make mount mtr netstat ping ps tail traceroute \
-               wdiff
+set -l execs cat cvs df diff dig gcc g++ ls ifconfig \
+       make mount mtr netstat ping ps tail traceroute \
+       wdiff blkid du dnf docker docker-compose docker-machine env id ip iostat journalctl kubectl \
+       last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
+       fdisk findmnt free semanage sar ss sysctl systemctl stat showmount \
+       tcpdump tune2fs vmstat w who sockstat
 
   if set -q grc_plugin_execs
     set execs $grc_plugin_execs
